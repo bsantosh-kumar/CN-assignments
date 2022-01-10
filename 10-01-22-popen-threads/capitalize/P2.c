44d3235f09e604153d35cf48972a4e0d377c6ca7
Santosh-Kumar <beerukurisantoshkumar@gmail.com>
2022-01-10 14:51:12 -0500

    done with 'capitalize'

---
+++ 10-01-22-popen-threads/capitalize/P2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#define SIZE 101
+int main()
+{
+    char *writeChar = "w";
+
+    FILE *writeFile = popen("./P3", writeChar);
+    int fd = fileno(writeFile);
+    int screen = dup(1);
+    dup2(fd, 1);
+    while (1)
+    {
+        char buffer[100];
+        scanf("%s", buffer);
+        printf("%s ", buffer);
+        fflush(stdout);
+    }
+}
---
+++ 10-01-22-popen-threads/capitalize/P3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#define SIZE 100
+int main()
+{
+    mkfifo("green", 0666);
+    int ffd = open("green", O_WRONLY);
+    int writeFD = ffd;
+    while (1)
+    {
+        char buffer[100];
+        scanf("%s", buffer);
+        int lenBuffer = strlen(buffer);
+        for (int i = 0; i < lenBuffer; i++)
+        {
+            if (buffer[i] >= 'a' && buffer[i] <= 'z')
+            {
+                buffer[i] = buffer[i] - 'a' + 'A';
+            }
+        }
+        printf("In P3 '%s'\n", buffer);
+        strcat(buffer, " from P3 ");
+        write(writeFD, buffer, lenBuffer + 10);
+    }
+}
